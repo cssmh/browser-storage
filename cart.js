a46c800a773899ed3b9b7811e08c25ec22595e2d
momen210 <touristmomen@gmail.com>
2023-12-13 23:37:54 +0600

    issue solve

--- cart.js
+++ cart.js
@@ -1,18 +1,48 @@
-const btnClick = () => {
-    const productName = document.getElementById("product-name");
-    const productQuantity = document.getElementById("product-quantity");
-    const name = productName.value;
-    const quantity = productQuantity.value;
-    productName.value = "";
-    productQuantity.value = "";
-    console.log(name, quantity);
-    displayProduct(name, quantity)
+const addProduct = () => {
+    const productField = document.getElementById('product-name');
+    const quantityField = document.getElementById('product-quantity');
+    const product = productField.value;
+    const quantity = quantityField.value;
+    productField.value='';
+    quantityField.value = '';
+
+
+    console.log(product, quantity);
+    displayProduct(product, quantity);
+    saveProductToLocalStorage(product, quantity);
 }
 
-const displayProduct = (name, quantity) => {
+const displayProduct = (product, quantity) =>{
+    const ul = document.getElementById('product-container');
     const li = document.createElement('li');
-    li.innerHTML = `${name}: ${quantity}`
-
-    const cont = document.getElementById("product-container");
-    cont.appendChild(li)
+    li.innerText = `${product}: ${quantity}`;
+    ul.appendChild(li);
 }
+
+const getStoredShoppingCart = () =>{
+    let cart = {};
+    const storedCart = localStorage.getItem('cart');
+    if(storedCart){
+        cart = JSON.parse(storedCart);
+    }
+    return cart;
+}
+
+const saveProductToLocalStorage = (product, quantity) => {
+    const cart = getStoredShoppingCart();
+    cart[product] = quantity;
+    const cartStringified = JSON.stringify(cart);
+    localStorage.setItem('cart', cartStringified)
+}
+
+const displayProductsFromLocalStorage = () => {
+    const savedCart = getStoredShoppingCart();
+    console.log(savedCart);
+    for(const product in savedCart){
+        const quantity = savedCart[product];
+        console.log(product, quantity);
+        displayProduct(product, quantity);
+    }
+}
+
+displayProductsFromLocalStorage();
